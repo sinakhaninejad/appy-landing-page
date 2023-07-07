30196b4e03b8e49d45088382dcbeb12b87897c27
Sina Khaninejad <sinakaya98765@gmail.com>
2023-07-07 16:23:10 +0330

    completed main section

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,55 @@ import { RiCloseFill } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaPlay } from "react-icons/fa";
 //pictures
-import HeroPicture from "./images/Col.png";
+import HeroPicture from "./images/hero-picture.png";
 import EssentialPicture from "./images/essential-sec-pic.png";
+import FocusPicture from "./images/focus-sec-pic.png";
+//carosel pictures
+import CaroselOne from "./images/carouselOne.png";
+import CaroselTwo from "./images/beautiful-young-female-doctor-looking-camera-office.jpg";
+import CaroselThree from "./images/portrait-beautiful-mature-blonde-bearded-guy-with-trendy-hairdo-casual-grey-shirt-smiling.jpg";
+import CaroselFour from "./images/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer.jpg";
 //svg import
 import RedCircle from "./images/red-circle.svg";
 import BlueCircle from "./images/blue-circle.svg";
 import YellowCircle from "./images/yellow-circle.svg";
 //hooks
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//slider moduales
+import { Navigation, Pagination, A11y } from "swiper/modules";
+//core
+import { Swiper, SwiperSlide } from "swiper/react";
+//slider styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function App() {
   const [navExtendted, setNavExtend] = useState(true);
+  const [screenSize, setScreenSize] = useState(getCurrentDimension());
   function extendNav() {
     setNavExtend(!navExtendted);
   }
+
+  function getCurrentDimension() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  useEffect(() => {
+    const updateDimension = () => {
+      setScreenSize(getCurrentDimension());
+    };
+    window.addEventListener("resize", updateDimension);
+
+    return () => {
+      window.removeEventListener("resize", updateDimension);
+    };
+  }, [screenSize]);
+
   return (
     <>
       <header className="border-solid border border-nav-border ">
@@ -82,11 +117,11 @@ function App() {
                   cras vel lacinia turpis volutpat adipiscing. Sollicitudin at
                   velit, blandit tempus nunc in.
                 </p>
-                <div className="flex items-center ">
-                  <button className="bg-main-button shrink-0 px-8 py-3  rounded-md cursor-pointer text-white text-2xl font-bold">
+                <div className="flex sm:flex-col items-center  ">
+                  <button className="bg-main-button whitespace-nowrap px-8 py-3  rounded-md cursor-pointer text-white text-2xl font-bold">
                     Get Started
                   </button>
-                  <button className="ml-10 shrink-0 text-main-button cursor-pointer font-medium text-xl flex items-center gap-3">
+                  <button className="sm:mt-7 whitespace-nowrap px-8 py-3 text-main-button cursor-pointer font-medium text-xl flex items-center gap-3">
                     <FaPlay />
                     Watch Video
                   </button>
@@ -138,23 +173,191 @@ function App() {
               Sollicitudin at velit, blandit tempus nunc in.
             </p>
             <div className="even-columns| mx-auto flex sm:flex-col gap-12 justify-between items-center">
-              <div className="w-1/2 flex justify-center">
+              <div className="sm:w-10/12 w-1/2 flex justify-center">
                 <img className=" " src={EssentialPicture} alt="" />
               </div>
-              <div className="w-1/2  ">
-                <h4 className="text-xl font-medium mb-8">Sem augue tempus</h4>
-                <p className="font-normal text-lg mb-8">
-                  Venenatis blandit habitasse nunc, sapien enim elit in. Arcu,
-                  pharetra, et cursus sit senectus in blandit. Aliquet enim
-                  fermentum non semper nibh ut neque. Pellentesque ut tincidunt
-                  vitae arcu bibendum malesuada lorem sapien volutpat.
-                </p>
-                <h4 className="text-xl font-medium mb-8">
+              <div className="sm:w-10/12 w-1/2  ">
+                <div className="pl-5 border-y-0 border-r-0 border-solid border-main-button">
+                  <h4 className="text-xl font-medium mb-8">Sem augue tempus</h4>
+                  <p className="font-normal text-lg mb-8 ">
+                    Venenatis blandit habitasse nunc, sapien enim elit in. Arcu,
+                    pharetra, et cursus sit senectus in blandit. Aliquet enim
+                    fermentum non semper nibh ut neque. Pellentesque ut
+                    tincidunt vitae arcu bibendum malesuada lorem sapien
+                    volutpat.
+                  </p>
+                </div>
+
+                <h4 className="text-xl font-medium mb-8 pl-5 border-y-0 border-r-0 border-solid border-[#CBD5E1]">
                   Habitant integer interdum a
                 </h4>
-                <h4 className="text-xl font-medium">Tempus natoque id</h4>
+                <h4 className="text-xl font-medium pl-5 border-y-0 border-r-0 border-solid border-[#CBD5E1]">
+                  Tempus natoque id
+                </h4>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="py-20 sm:py-12">
+          <div className="container| w-11/12 mx-auto ">
+            <h2 className="text-center text-5xl sm:text-3xl font-extrabold sm:font-bold ">
+              Focus on what matters
+            </h2>
+            <p className="text-lg text-center mt-7 mb-20">
+              Odio vulputate cras vel lacinia turpis volutpat adipiscing.
+              Sollicitudin at velit, blandit tempus nunc in.
+            </p>
+            <div className="even-columns| mx-auto flex sm:flex-col gap-12 justify-between items-center">
+              <div className="sm:w-10/12 w-1/2 flex justify-center">
+                <img className=" " src={FocusPicture} alt="" />
+              </div>
+              <div className="sm:w-10/12 w-1/2  ">
+                <div className="pl-5 border-y-0 border-r-0 border-solid border-main-button">
+                  <h4 className="text-xl font-medium mb-8">
+                    Bibendum gravida dolor egestas aliquam
+                  </h4>
+                  <p className="font-normal text-lg mb-8 ">
+                    Egestas tincidunt hendrerit nibh platea sit vivamus aenean
+                    rhoncus etiam. Tristique amet, sed ac ac dolor habitant.
+                    Mauris duis neque molestie venenatis nibh hendrerit
+                    pharetra. Tortor suscipit leo sit tellus ac scelerisque
+                    gravida sem.
+                  </p>
+                </div>
+
+                <h4 className="text-xl font-medium mb-8 pl-5 border-y-0 border-r-0 border-solid border-[#CBD5E1]">
+                  Egestas lorem eget
+                </h4>
+                <h4 className="text-xl font-medium pl-5 border-y-0 border-r-0 border-solid border-[#CBD5E1]">
+                  Tellus eget feugiat sit
+                </h4>
+              </div>
+            </div>
+            <div className="flex justify-center py-20 sm:py-12">
+              <button className="bg-main-button text-white cursor-pointer rounded-md px-8 py-3 ">
+                Get Started
+              </button>
+            </div>
+          </div>
+        </section>
+        {/* ******************swiper************** */}
+        <section className="bg-[#F1F5F9] py-20 sm:py-12">
+          <div className="container| w-11/12 mx-auto ">
+            <Swiper
+              modules={[Navigation, Pagination, A11y]}
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation={screenSize.width >= 640 ? true : false}
+              pagination={{ clickable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log("slide change")}
+            >
+              <SwiperSlide>
+                <div className="flex sm:flex-col justify-around items-center">
+                  <img src={CaroselOne} className="sm:w-10/12 w-1/2 max-w-xl" />
+                  <div className="sm:w-10/12 sm:mx-auto sm:text-center w-1/2 ml-10 max-w-xl">
+                    <h4 className="font-semibold text-xl tracking-wider text-footer-gray mt-8">
+                      SUCCESS STORIES
+                    </h4>
+                    <p className="font-normal text-2xl my-10 tracking-wide">
+                      <span className="text-[#EAB308]">“</span> Id urna, nisl,
+                      ut quam. Diam suspendisse fringilla quam arcu mattis est
+                      velit in. Nibh in purus sit convallis phasellus ut. At vel
+                      erat ultricies commodo. Neque suspendisse a habitasse
+                      commodo. <span className="text-[#EF4444]">”</span>
+                    </p>
+                    <h4 className="text-lg font-normal">
+                      Marie Poirot,
+                      <span className="font-medium">
+                        <br />
+                        Bigapp
+                      </span>
+                    </h4>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="flex sm:flex-col justify-around items-center">
+                  <img
+                    src={CaroselTwo}
+                    className="sm:w-10/12 w-1/2 rounded-lg max-w-xl"
+                  />
+                  <div className="sm:w-10/12 sm:mx-auto sm:text-center w-1/2 ml-10 max-w-xl">
+                    <h4 className="font-semibold text-xl tracking-wider text-footer-gray">
+                      SUCCESS STORIES
+                    </h4>
+                    <p className="font-normal text-2xl my-10 tracking-wide">
+                      <span className="text-[#EAB308]">“</span> Id urna, nisl,
+                      ut quam. Diam suspendisse fringilla quam arcu mattis est
+                      velit in. Nibh in purus sit convallis phasellus ut. At vel
+                      erat ultricies commodo. Neque suspendisse a habitasse
+                      commodo. <span className="text-[#EF4444]">”</span>
+                    </p>
+                    <h4 className="text-lg font-normal">
+                      Marie Poirot,
+                      <span className="font-medium">
+                        <br />
+                        Bigapp
+                      </span>
+                    </h4>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="flex sm:flex-col justify-around items-center">
+                  <img
+                    src={CaroselThree}
+                    className="sm:w-10/12 w-1/2 max-w-xl rounded-lg"
+                  />
+                  <div className="sm:w-10/12 sm:mx-auto sm:text-center w-1/2 ml-10 max-w-xl">
+                    <h4 className="font-semibold text-xl tracking-wider text-footer-gray">
+                      SUCCESS STORIES
+                    </h4>
+                    <p className="font-normal text-2xl my-10 tracking-wide">
+                      <span className="text-[#EAB308]">“</span> Id urna, nisl,
+                      ut quam. Diam suspendisse fringilla quam arcu mattis est
+                      velit in. Nibh in purus sit convallis phasellus ut. At vel
+                      erat ultricies commodo. Neque suspendisse a habitasse
+                      commodo. <span className="text-[#EF4444]">”</span>
+                    </p>
+                    <h4 className="text-lg font-normal">
+                      Marie Poirot,
+                      <span className="font-medium">
+                        <br />
+                        Bigapp
+                      </span>
+                    </h4>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="flex sm:flex-col justify-around items-center">
+                  <img
+                    src={CaroselFour}
+                    className="sm:w-10/12 w-1/2 max-w-xl rounded-lg"
+                  />
+                  <div className="sm:w-10/12 sm:mx-auto sm:text-center w-1/2 ml-10 max-w-xl">
+                    <h4 className="font-semibold text-xl tracking-wider text-footer-gray">
+                      SUCCESS STORIES
+                    </h4>
+                    <p className="font-normal text-2xl my-10 tracking-wide">
+                      <span className="text-[#EAB308]">“</span> Id urna, nisl,
+                      ut quam. Diam suspendisse fringilla quam arcu mattis est
+                      velit in. Nibh in purus sit convallis phasellus ut. At vel
+                      erat ultricies commodo. Neque suspendisse a habitasse
+                      commodo. <span className="text-[#EF4444]">”</span>
+                    </p>
+                    <h4 className="text-lg font-normal">
+                      Marie Poirot,
+                      <span className="font-medium">
+                        <br />
+                        Bigapp
+                      </span>
+                    </h4>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </section>
       </main>
